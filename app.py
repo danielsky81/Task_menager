aefc97725eac58a74024af92809620c87f13ec71
Daniel Orchel <daniel.orchel@yahoo.com>
2019-02-23 23:31:50 +0000

    Add missing method render_template

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = 'mongodb+srv://daniel:r00tUser@myfirstcluster-ggk14.mongodb.net/task_manager?retryWrites=true'
 
 mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template('tasks.html', tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=False)
+                                port=int(os.environ.get('PORT')),
+                                debug=True)
